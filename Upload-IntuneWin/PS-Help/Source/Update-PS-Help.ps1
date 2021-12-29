00ded8d31cd50f1da61231a5e33eb40e670b98a9
Fabian Niesen <mail@fabian-niesen.de>
2021-12-29 17:12:27 +0100

    Added PS-Help

---
+++ Upload-IntuneWin/PS-Help/Source/Update-PS-Help.ps1
@@ -0,0 +1,2 @@
+﻿Save-Help -DestinationPath $PSScriptRoot\PS-Help\de-DE -Module $(Get-Module -ListAvailable) -UICulture de-DE -ErrorAction SilentlyContinue
+Save-Help -DestinationPath $PSScriptRoot\PS-Help\en-US -Module $(Get-Module -ListAvailable) -UICulture en-US -ErrorAction SilentlyContinue
